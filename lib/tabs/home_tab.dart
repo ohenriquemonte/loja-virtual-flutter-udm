759934fa5c8a5ace16185d310a6b1c60de8c4c17
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-04-13 17:46:18 -0300

    136. Grade de Imagens

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -38,6 +40,8 @@ class HomeTab extends StatelessWidget {
                   .orderBy('pos')
                   .getDocuments(),
               builder: (context, snapshot) {
+                print('snapshot.hasData ${snapshot.hasData}');
+
                 if (!snapshot.hasData)
                   return SliverToBoxAdapter(
                     child: Container(
@@ -51,14 +55,25 @@ class HomeTab extends StatelessWidget {
                 else {
                   print(snapshot.data.documents.length);
 
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200,
-                      alignment: Alignment.center,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                      ),
-                    ),
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map(
+                      (doc) {
+                        return StaggeredTile.count(
+                          doc.data['x'],
+                          doc.data['y'],
+                        );
+                      },
+                    ).toList(),
+                    children: snapshot.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.data['image'],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
                 }
               },
