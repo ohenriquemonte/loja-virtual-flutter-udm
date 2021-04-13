db9ae333ea6c843530604454bee37b6fb8c7cc92
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-04-13 17:22:17 -0300

    135. Obtendo as Imagens da Tela Inicial

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,39 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        Widget _buildBodyBack() => Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [
-                        Color.fromARGB(255, 211, 118, 130),
-                        Color.fromARGB(255, 253, 181, 168),
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                ),
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildBodyBack() => Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 211, 118, 130),
+                Color.fromARGB(255, 253, 181, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
+          ),
         );
-        
-        return Stack(
-            children: [
-                _buildBodyBack(),
-                CustomScrollView(
-                    slivers: [
-                        SliverAppBar(
-                            floating: true,
-                            snap: true,
-                            backgroundColor: Colors.transparent,
-                            elevation: 0.0,
-                            flexibleSpace: FlexibleSpaceBar(
-                                title: const Text('Novidades'),
-                                centerTitle: true,
-                            ),
-                        ),
-                    ],
-                ),
-            ],
-        );
-    }
+
+    return Stack(
+      children: [
+        _buildBodyBack(),
+        CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text('Novidades'),
+                centerTitle: true,
+              ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection('home')
+                  .orderBy('pos')
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                else {
+                  print(snapshot.data.documents.length);
+
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
