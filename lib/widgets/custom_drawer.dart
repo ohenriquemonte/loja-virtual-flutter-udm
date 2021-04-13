b448160c4dd1ec7fa595c81a5143656b2b3003ba
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-04-13 17:57:15 -0300

    137. Criando um Drawer Customizado

---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildDrawerBack() => Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 203, 236, 241),
+                Colors.white,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        );
+
+    return Drawer(
+      child: Stack(
+        children: [
+          _buildDrawerBack(),
+          ListView(
+            padding: const EdgeInsets.only(left: 32.0, top: 16.0),
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 8.0),
+                padding: EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 8.0),
+                height: 170.0,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      child: Text(
+                        "Flutter's\nClothing",
+                        style: TextStyle(
+                          fontSize: 34.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      top: 8.0,
+                      left: 0.0,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
