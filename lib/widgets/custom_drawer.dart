45d3f5074354e5c2ec541b76441cda475ba157e6
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-04-14 16:52:21 -0300

    138. Itens do Drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
@@ -39,10 +40,40 @@ class CustomDrawer extends StatelessWidget {
                       ),
                       top: 8.0,
                       left: 0.0,
-                    )
+                    ),
+                    Positioned(
+                      left: 0.0,
+                      bottom: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Olá, ',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              'Entre ou cadastre-se >',
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onTap: () {},
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
+              Divider(),
+              DrawerTile(Icons.home, 'Início'),
+              DrawerTile(Icons.list, 'Produtos'),
+              DrawerTile(Icons.location_on, 'Lojas'),
+              DrawerTile(Icons.playlist_add_check, 'Meus Pedidos'),
             ],
           ),
         ],
