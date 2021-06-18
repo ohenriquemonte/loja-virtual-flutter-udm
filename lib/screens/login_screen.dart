15b28ccca4a900083940f1bf00f3b0a7dd4121ae
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-06-18 17:27:03 -0300

    152. Introdução ao ScopedModel

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/signup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
@@ -31,69 +33,79 @@ class LoginScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: [
-            TextFormField(
-              decoration: InputDecoration(hintText: 'E-mail'),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty || !text.contains('@'))
-                  return "E-mail inválido!";
-                return null;
-              },
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Senha'),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) return "Senha inválida!";
-                return null;
-              },
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(top: 12.0),
-                child: InkWell(
-                  onTap: () {},
-                  child: Text(
-                    "Esqueci minha senha",
-                    style: TextStyle(fontWeight: FontWeight.w400),
-                    textAlign: TextAlign.right,
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(child: CircularProgressIndicator());
+
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty || !text.contains('@'))
+                      return "E-mail inválido!";
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Senha'),
+                  obscureText: true,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6)
+                      return "Senha inválida!";
+                    return null;
+                  },
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 12.0),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "Esqueci minha senha",
+                        style: TextStyle(fontWeight: FontWeight.w400),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            SizedBox(
-              height: 44.0,
-              child: ElevatedButton(
-                child: Text(
-                  'Entrar',
-                  style: TextStyle(
-                    fontSize: 18,
+                SizedBox(height: 16.0),
+                SizedBox(
+                  height: 44.0,
+                  child: ElevatedButton(
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                        (materialState) => Theme.of(context).primaryColor,
+                      ),
+                      foregroundColor: MaterialStateProperty.resolveWith(
+                        (materialState) => Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {}
+
+                      model.signIn();
+                    },
                   ),
-                  textAlign: TextAlign.right,
                 ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith(
-                    (materialState) => Theme.of(context).primaryColor,
-                  ),
-                  foregroundColor: MaterialStateProperty.resolveWith(
-                    (materialState) => Colors.white,
-                  ),
-                ),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
