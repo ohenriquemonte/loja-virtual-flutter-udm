4b5d433518385308fd88537e5e47e0b5def8ccc9
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-06-18 17:00:33 -0300

    151. Tela de Cadastro

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,47 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:loja_virtual/screens/signup_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class SignUpScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Entrar'),
-        centerTitle: true,
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => SignUpScreen(),
-                ),
-              );
-            },
-            child: Text(
-              'CRIAR CONTA',
-              style: TextStyle(fontSize: 15.0),
-            ),
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.resolveWith(
-                (materialState) => Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text('Criar Conta'), centerTitle: true),
       body: Form(
         key: _formKey,
         child: ListView(
           padding: EdgeInsets.all(16.0),
           children: [
             TextFormField(
+              decoration: InputDecoration(hintText: 'Nome Completo'),
+              validator: (text) {
+                if (text.isEmpty) return "Nome Completo inválido!";
+                return null;
+              },
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
               decoration: InputDecoration(hintText: 'E-mail'),
               keyboardType: TextInputType.emailAddress,
               validator: (text) {
                 if (text.isEmpty || !text.contains('@'))
-                  return "E-mail inválido!";
+                  return "E-mail inválido";
                 return null;
               },
             ),
@@ -54,26 +38,20 @@ class LoginScreen extends StatelessWidget {
                 return null;
               },
             ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(top: 12.0),
-                child: InkWell(
-                  onTap: () {},
-                  child: Text(
-                    "Esqueci minha senha",
-                    style: TextStyle(fontWeight: FontWeight.w400),
-                    textAlign: TextAlign.right,
-                  ),
-                ),
-              ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Endereço'),
+              validator: (text) {
+                if (text.isEmpty) return "Endereço inválido!";
+                return null;
+              },
             ),
             SizedBox(height: 16.0),
             SizedBox(
               height: 44.0,
               child: ElevatedButton(
                 child: Text(
-                  'Entrar',
+                  'Criar Conta',
                   style: TextStyle(
                     fontSize: 18,
                   ),
