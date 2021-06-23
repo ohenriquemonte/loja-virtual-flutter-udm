44c955a444ddf04b89765a3e2534d0a3a0c7e4a0
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-06-23 00:23:40 -0300

    168. Criando o Resumo do Pedido

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
+import 'package:loja_virtual/widgets/cart_price.dart';
 import 'package:loja_virtual/widgets/discount_card.dart';
 import 'package:loja_virtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -90,6 +91,7 @@ class CartScreen extends StatelessWidget {
               ),
               DiscountCard(),
               ShipCard(),
+              CartPrice(() {}),
             ],
           );
         }
