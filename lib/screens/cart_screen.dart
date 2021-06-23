c3c52f2824383ad24edbdffd2d17a0d3992a15fa
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-06-23 00:09:23 -0300

    167. Criando a Card do Frete

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -4,6 +4,7 @@ import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
 import 'package:loja_virtual/widgets/discount_card.dart';
+import 'package:loja_virtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -88,6 +89,7 @@ class CartScreen extends StatelessWidget {
                 ).toList(),
               ),
               DiscountCard(),
+              ShipCard(),
             ],
           );
         }
