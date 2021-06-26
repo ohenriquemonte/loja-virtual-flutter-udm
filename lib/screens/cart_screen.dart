dff3b7b0a0815658fdeb13e0c229b97c51b83e96
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2021-06-26 17:41:24 -0300

    171. Tela de Confirmação do Pedido

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
+import 'package:loja_virtual/screens/order_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
 import 'package:loja_virtual/widgets/cart_price.dart';
 import 'package:loja_virtual/widgets/discount_card.dart';
@@ -96,6 +97,10 @@ class CartScreen extends StatelessWidget {
 
                 if (orderId != null) {
                   print('Pedido $orderId finalizado com sucesso!');
+
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => OrderScreen(orderId),
+                  ));
                 }
               }),
             ],
