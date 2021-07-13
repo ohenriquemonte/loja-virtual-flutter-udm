52c5b198bf2c34316cbe91b92c7f3cac695b05f4
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-07-13 15:14:17 -0300

    176. Acompanhando o Status do Pedido

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -23,6 +23,8 @@ class OrderTile extends StatelessWidget {
             if (!snapshot.hasData) {
               return Center(child: CircularProgressIndicator());
             } else {
+              int status = snapshot.data["status"];
+
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -33,6 +35,24 @@ class OrderTile extends StatelessWidget {
                   SizedBox(height: 16.0),
                   Text(
                     _buildProductsText(snapshot.data),
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(
+                    'Status do Pedido:',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 4.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildCircle("1", "Preparação", status, 1),
+                      Container(
+                          height: 1.0, width: 40.0, color: Colors.grey[500]),
+                      _buildCircle("2", "Transporte", status, 2),
+                      Container(
+                          height: 1.0, width: 40.0, color: Colors.grey[500]),
+                      _buildCircle("3", "Entrega", status, 3),
+                    ],
                   ),
                 ],
               );
@@ -52,5 +72,46 @@ class OrderTile extends StatelessWidget {
     }
     text += 'Total: R\$ ${snapshot.data['totalPrice'].toStringAsFixed(2)}';
     return text;
+  }
+
+  Widget _buildCircle(
+      String title, String subtitle, int status, int thisStatus) {
+    Color backColor;
+    Widget child;
+
+    if (status < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      );
+    } else if (status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(
+        Icons.check,
+        color: Colors.white,
+      );
+    }
+
+    return Column(
+      children: [
+        CircleAvatar(radius: 20, backgroundColor: backColor, child: child),
+        Text(subtitle),
+      ],
+    );
   }
 }
